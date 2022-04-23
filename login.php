3d89eb072db06e89052d279c03565edc5275013a
Kali <tarzanvn>
2022-04-23 11:26:01 -0400

    update title login

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if ($user === "admin" and $pass === "admin"){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login V4</title>
+	<title>Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
